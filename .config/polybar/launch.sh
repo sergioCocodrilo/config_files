4553ad55d689a59c067c01ba8390465d4bd34763
Sergio Solano <sergio.solano.p@gmail.com>
2023-03-15 19:51:46 -0600

    polybar in second screen and smart gaps for i3 enabled

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,6 +9,13 @@ polybar-msg cmd quit
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
-# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar secondmonitor 2>&1 | tee -a /tmp/polybar2.log & disown
+# if type "xrandr"; then
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#     MONITOR=$m polybar --reload mybar &
+#   done
+# else
+#   polybar --reload mybar &
+# fi
 
 echo "Bars launched..."
