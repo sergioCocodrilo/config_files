6f3fb5f408d8c6045d37685d0aeff9951dd92952
Sergio Solano <sergio.solano.p@gmail.com>
2024-02-17 12:05:45 -0600

    nvim configs to work with kitty, tree-sitter and lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -44,7 +44,10 @@ require("lazy").setup({
 	  'nvim-tree/nvim-tree.lua',
 	  dependencies = 'nvim-tree/nvim-web-devicons',
 	  tag = 'nightly' -- optional, updated every week. (see issue #1193)
-	}
+	},
+    'kamykn/spelunker.vim',
+    'godlygeek/tabular',
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
 })
 
 -- Transparency
@@ -85,7 +88,34 @@ vim.opt.termguicolors = true
 require("nvim-tree").setup()
 -- require('nvim_comment').setup()
 
+
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  -- List of parsers to ignore installing (or "all")
+  --ignore_install = { "javascript" },
+
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  indent = {enable = true}
+}
+
+
+require("ibl").setup()
+
+------------------------------------------------------------
 -- mappings
+------------------------------------------------------------
 local function map(mode, key, value)
 	vim.keymap.set(mode, key, value, {silent = true})
 end
